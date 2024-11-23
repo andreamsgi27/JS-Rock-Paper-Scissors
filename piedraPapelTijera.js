9274c056ffa4569d6dcba71cdbc16bed1fec2623
andreamsgi27 <andreamsgi27@hotmail.com>
2024-11-23 12:42:03 +0100

    add listener

--- piedraPapelTijera.js
+++ piedraPapelTijera.js
@@ -2,3 +2,12 @@
 var posibilidades = ["piedra", "papel", "tijera"];
 //    //
 
+
+const boton = document.querySelector("button");
+boton.addEventListener('click', () => {
+    const inputNombre = document.getElementsByName("nombre")[0];
+    const inputPartidas = document.getElementsByName("partidas")[0];
+});
+
+
+
